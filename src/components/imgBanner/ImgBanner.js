0a7970309b5917200d1fc576608a07b51dbd5104
swethaogeti <65771591+swethaogeti@users.noreply.github.com>
2022-09-23 01:00:07 +0530

    fix : pathname

--- src/components/imgBanner/ImgBanner.js
+++ src/components/imgBanner/ImgBanner.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./imgBanner.css";
 import HistoryIcon from "@material-ui/icons/HistoryOutlined";
+import { useLocation } from "react-router-dom";
 
 const ImgBanner = ({ imgBanner }) => {
+  const { pathname } = useLocation();
+  console.log(pathname);
   return (
     <div className="imgBanner">
       <div className="img-display">
@@ -10,7 +13,10 @@ const ImgBanner = ({ imgBanner }) => {
       </div>
       <div className="grey-overlay">
         <HistoryIcon />
-        <h4>History</h4>
+
+        {pathname === "/history" && <h4>History</h4>}
+        {pathname === "/likes" && <h4>Likes</h4>}
+        {pathname === "/watchlater" && <h4>watchlater</h4>}
       </div>
     </div>
   );
